2162f0d3d59f68f9822cffa0de1f69bbaf0d3dae
Zahra Amani <95489276+zahraamani7065@users.noreply.github.com>
2023-06-15 23:33:43 +0430

    add models

---
+++ app/src/main/java/android/company/aronlineshop/models/Product.kt
@@ -0,0 +1,14 @@
+package android.company.aronlineshop.models
+
+data class Product
+    (
+    val id: Int,
+    val name: String,
+    val price: String,
+    val imageId: Int,
+    val delivery: String,
+    val longDescription: String,
+    val rating: Float,
+    val ratingCount: String,
+    val modelURL: String
+)
